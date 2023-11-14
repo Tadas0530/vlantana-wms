d6b927bf019042dab0a5c2fcb4f1cb228e3aa855
Tadas Andrijauskas <tadasandrijauskass@gmail.com>
2023-11-14 19:07:55 +0200

    - database config

--- vlantana-wms-backend-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ vlantana-wms-backend-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('company_id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role');
+            $table->string('phone_number');
+            $table->string('photo_path');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- vlantana-wms-backend-laravel/routes/api.php
+++ vlantana-wms-backend-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('api')->prefix('v2')->group(function () {
+Route::middleware('api')->prefix('v1')->group(function () {
 
     // auth
     Route::post('/register', [AuthController::class, 'register'])->name("register");
